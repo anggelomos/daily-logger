754dd2dbc77966112a4c1cb3ba2011e762462735
anggelomos <60065757+anggelomos@users.noreply.github.com>
2024-01-01 11:38:54 +0000

    Improved logs format

--- main.py
+++ main.py
@@ -17,26 +17,28 @@ def format_logs(logs: List[Task], logs_date: str) -> str:
         A string representing the formatted logs. Each task is represented by a line that includes the
         task's creation time (in 12-hour format) and title. For example "08:40 AM This is just a day log"
     """
-    filtered_logs = [log for log in logs if log.created_date.startswith(logs_date)]
+    filtered_logs = [
+        log for log in logs if log.created_date.startswith(logs_date)
+    ]
     sorted_logs = sorted(filtered_logs, key=lambda x: x.created_date)
-    return "\n".join(
-        [
-            f"{datetime.fromisoformat(log.created_date).strftime('%I:%M %p')} {log.title}"
-            for log in sorted_logs
-        ]
-    )
+    return "\n".join([
+        f"- {datetime.fromisoformat(log.created_date).strftime('%-I:%M %p').lower()} {log.title}"
+        for log in sorted_logs
+    ])
 
 
 def main():
-    delta_days = 0
-    ticktick_client = TicktickClient(os.getenv("TT_USER"), os.getenv("TT_PASS"))
+    delta_days = -1
+    ticktick_client = TicktickClient(os.getenv("TT_USER"),
+                                     os.getenv("TT_PASS"))
 
     quebec_timezone = ZoneInfo("America/Toronto")
-    logs_date = (datetime.now(quebec_timezone) + timedelta(days=delta_days)).strftime("%Y-%m-%d")
+    logs_date = (datetime.now(quebec_timezone) +
+                 timedelta(days=delta_days)).strftime("%Y-%m-%d")
 
-    print("Day logs")
+    print("\n### 📟 Day logs")
     print(format_logs(ticktick_client.get_day_logs(), logs_date))
-    print("\nDay highlights")
+    print("\n### ✨ Day highlights")
     print(format_logs(ticktick_client.get_day_highlights(), logs_date))
 
 
